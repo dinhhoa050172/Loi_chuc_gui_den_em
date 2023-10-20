c378c2feab4f7d915324f8136ac76dcf1fe43c02
dinhhoa050172 <117873915+dinhhoa050172@users.noreply.github.com>
2023-10-20 07:29:04 +0700

    Add files via upload
    create

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = 'Em yêu của anh';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Tặng em yêu';
+const contentCard = 'Hôm nay thật là một ngày đặc biệt, anh chúc em luôn vui vẻ và nở những nụ cười đẹp nhất trong ngày hôm nay. Anh sẽ cố gắng là người mang tới những nụ cười thật tươi ấy cho em, không chỉ hôm nay mà cho tới hết cả cuộc đời. Chúc em một ngày 20/10 vui vẻ nhé!';
+
+const giftImage = 'anhemyeu.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
---
+++ present.js
@@ -0,0 +1,31 @@
+var to = nameGirl;
+var gift_image_url = giftImage || giftImageBase64;
+
+var nametag = document.getElementById("nametag");
+var present = document.getElementById("present");
+var presentImage = document.getElementById("present-image");
+
+function init() {
+  var graphElem = document.querySelector('.present-box > .side.top .to');
+  graphElem.setAttribute('data-before', eventName);
+  document.querySelector('#card .title-card').innerHTML = `💘${titleCard}💘`;
+  document.querySelector('#card .content-card').innerHTML = `${contentCard}`;
+  document.querySelector('#card .honey').setAttribute('src', `${giftImage}`);
+
+  var _giftImg;
+  
+  if (gift_image_url) {
+    _giftImg = document.createElement("img");
+    _giftImg.src = gift_image_url;
+    presentImage.appendChild(_giftImg);
+  }
+    
+  present.addEventListener("click", function(e){
+    present.classList.toggle("open");
+    document.getElementById('card').classList.add('card-show');
+  }, false);
+  
+  nametag.innerText = to;
+}
+
+init();
